59f2339d879b40034daa23848327682cdefe9ae9
Bruno S M <misterselectmice@gmail.com>
2025-10-07 07:41:53 -0500

    Update main.c

--- prototipo_personagem/main.c
+++ prototipo_personagem/main.c
@@ -6,12 +6,13 @@ int AUX_WaitEventTimeout(SDL_Event* evt, Uint32* ms) {
     int ret = SDL_WaitEventTimeout(evt, *ms);
     Uint32 depois = SDL_GetTicks();
     Uint32 d = depois - antes;
-
+  
+    if (ret) {
     if (d >= *ms)
         *ms = 0;
     else
         *ms -= d;
-
+    }
     return ret;
 }
 
@@ -34,7 +35,7 @@ int main (int argc, char* args[]) {
 
     SDL_Rect r = { 180, 130, 20, 20 };
     bool rodando = true;
-    Uint32 espera = 100;
+    Uint32 espera = 10;
     SDL_Event evt;
 
     EstadoMovimento estado = PARADO;
@@ -145,6 +146,7 @@ int main (int argc, char* args[]) {
             default:
                 break;
         }
+        espera = 10;
 
         SDL_SetRenderDrawColor(ren, 255, 255, 255, 255);
         SDL_RenderClear(ren);
